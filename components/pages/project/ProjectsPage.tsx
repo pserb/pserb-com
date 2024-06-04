8836889b04eaa84daa183bbbb69eacb6860cb1bd
Paul Serbanescu <paulserbanescu3@gmail.com>
2024-06-04 03:44:52 -0400

    add back breadcrumb

--- components/pages/project/ProjectsPage.tsx
+++ components/pages/project/ProjectsPage.tsx
@@ -1,7 +1,7 @@
 import { ProjectPayload, ProjectsPagePayload } from '@/types'
 import { EncodeDataAttributeCallback } from '@sanity/react-loader'
 import { Card } from '@sanity/ui'
-import ProjectsPageComponent from './projects-page-component'
+import ProjectsPageComponent, { ProjectsPageBreadcrumb } from './projects-page-component'
 
 export interface ProjectsPageProps {
   data: ProjectsPagePayload | null
@@ -15,21 +15,24 @@ export default function ProjectsPage({
   const { projectList = [] } = data ?? {}
 
   return (
-    <div className="flex flex-col mt-6 space-y-12">
-      {projectList &&
-        projectList.map((project: ProjectPayload) => {
-          if (project) {
-            return (
-              <ProjectsPageComponent
-                link={project.slug!}
-                heading={project.heading!}
-                subheading={project.subheading!}
-                blurb={project.blurb!}
-              />
-            )
-          }
-          return null;
-        })}
-    </div>
+    <>
+    <ProjectsPageBreadcrumb />
+      <div className="flex flex-col mt-6 space-y-12">
+        {projectList &&
+          projectList.map((project: ProjectPayload) => {
+            if (project) {
+              return (
+                <ProjectsPageComponent
+                  link={project.slug!}
+                  heading={project.heading!}
+                  subheading={project.subheading!}
+                  blurb={project.blurb!}
+                />
+              )
+            }
+            return null
+          })}
+      </div>
+    </>
   )
 }
